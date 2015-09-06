b3c4e3023568e1ff6995590f1fe32654b6177a6f
Samuel Rivas <samuelrivas@gmail.com>
2015-09-06 14:41:56 +0200

    Fixed version of jam

--- custom-pkgs.nix
+++ custom-pkgs.nix
@@ -10,6 +10,8 @@ let
 
     udp-cat-debug = callPackage ./pkgs/udp-cat { debug = true; };
     udp-cat       = callPackage ./pkgs/udp-cat { debug = false; };
+
+    jam       = callPackage ./pkgs/jam { };
   };
 in
 self
