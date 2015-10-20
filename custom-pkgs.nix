a26b97c8e4fc3e212b6fb9a7f9436ae861ab2380
Samuel Rivas <samuel.rivas@schibsted.com>
2015-10-20 14:35:27 +0200

    Add some comments and use packages already upstream

--- custom-pkgs.nix
+++ custom-pkgs.nix
@@ -14,18 +14,20 @@ let
     jam       = callPackage ./pkgs/jam { };
     argyllcms = callPackage ./pkgs/argyllcms { };
 
+    # Waiiting for the fix to make it to the channels. Merged the 2015-09-07 in
+    # b70ffedbccc9ae2595e7cb1bbde66fbde5db8051
     colorThemeSolarized = callPackage ./pkgs/color-theme-solarized {
       inherit (pkgs.emacs24Packages) colorTheme;
     };
 
+    # Waiting for it to make to the channes, merged the 2015-09-07 in
+    # 54bcc4e44670dea71f8df2a1833744f1efc98190
     erlangMode = callPackage ./pkgs/erlang-mode { };
-
-    tuaregMode = callPackage ./pkgs/tuareg { };
 
     # TODO:
     #  * Read the config for this from a file a-la configuration.nix
     samuelEmacsConfig = callPackage ./pkgs/my-emacs-config {
-      inherit (pkgs.emacs24Packages) haskellMode;
+      inherit (pkgs.emacs24Packages) haskellMode tuaregMode;
       inherit (pkgs.ocamlPackages_4_02_1) merlin ocpIndent;
 
       utop = self.utop;
